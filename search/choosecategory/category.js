830e2316fd7931587e417ecf2b23f44bd76497a0
ParkSang-Hyeon <tkdgus4784@kaist.ac.kr>
2019-05-08 16:14:09 +0900

    sanghyeon search

---
+++ search/choosecategory/category.js
@@ -0,0 +1,73 @@
+var sf = document.getElementById("fantasy");
+var horror = document.getElementById("horror");
+var crime = document.getElementById("crime");
+var biography = document.getElementById("biography");
+var mystery = document.getElementById("mystery");
+var art = document.getElementById("artistic");
+var comedy = document.getElementById("comedy");
+var music = document.getElementById("music");
+var drama = document.getElementById("drama");
+var docu = document.getElementById("documentary");
+var action = document.getElementById("action");
+var romance = document.getElementById("romance");
+
+
+sf.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    sf.style.backgroundImage = "url('SF_hover.png')";
+});
+
+horror.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    horror.style.backgroundImage = "url('horror_hover.png')";
+});
+
+crime.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    crime.style.backgroundImage = "url('crime_hover.png')";
+});
+
+biography.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    biography.style.backgroundImage = "url('bio_hover.png')";
+});
+
+mystery.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    mystery.style.backgroundImage = "url('mystery_hover.png')";
+});
+
+art.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    art.style.backgroundImage = "url('art_hover.png')";
+});
+
+comedy.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    comedy.style.backgroundImage = "url('comedy_hover.png')";
+});
+
+music.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    music.style.backgroundImage = "url('music_hover.png')";
+});
+
+drama.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    drama.style.backgroundImage = "url('drama_hover.png')";
+});
+
+docu.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    docu.style.backgroundImage = "url('docu_hover.png')";
+});
+
+action.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    action.style.backgroundImage = "url('action_hover.png')";
+});
+
+romance.addEventListener('mouseover', function (e) {
+    console.log("profile hover out");
+    romance.style.backgroundImage = "url('romance_hover.png')";
+});
