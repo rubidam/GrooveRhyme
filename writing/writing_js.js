681f32845cbe423586f952d188201d6fce395eb2
Hansung Bae <bhs1124@kaist.ac.kr>
2019-05-09 17:07:51 +0900

    writing page ok btn make

--- writing/writing_js.js
+++ writing/writing_js.js
@@ -1,8 +1,12 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
+var textbox = document.getElementById("search_input");
+
+next_move = "./aspect/aspects.html?"
 
 function okPressListener(){
-	location.href = "./aspect/aspects.html";
+	next_move = next_move + "name=" + textbox.value;
+	location.href = next_move;
 };
 function backspace(){
 	location.href = javascript.history.back();
