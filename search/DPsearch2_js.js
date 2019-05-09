85473d074facdff4fdbeef5ee4ff561e034fdf11
Lee JeongJun <djfelrl11@kaist.ac.kr>
2019-05-09 16:49:10 +0900

    convey DPsearch2 value to chooseaspect

--- search/DPsearch2_js.js
+++ search/DPsearch2_js.js
@@ -1,2 +1,26 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
+
+function extractquery(loc){
+	var query = ""
+	var queryflag = 0;
+	for (var i= 0; i< loc.length; i++){
+		if(queryflag == 1){
+			query = query + loc[i];
+		}
+		if (loc[i] == '?') queryflag = 1;
+	}
+	return query;
+}
+
+function aspectPressListener(){
+	var nextloc = "./searchaspect/searchaspect.html?"
+	var loc = document.location.href;
+	var query = extractquery(loc);
+	nextloc = nextloc + query;
+	location.href = nextloc;
+}
+
+function skipPressListener(){
+	
+}
