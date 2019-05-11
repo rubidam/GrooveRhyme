99e2fedaf5f56d5d97e162c5e608fc96e47783c7
Hansung Bae <bhs1124@kaist.ac.kr>
2019-05-11 15:31:04 +0900

    fix index4

--- main_js.js
+++ main_js.js
@@ -1,21 +1,6 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDDFiULsphXfbs8XMTaJr4o4dxCPvCyX8w",
-    authDomain: "movielist-454ba.firebaseapp.com",
-    databaseURL: "https://movielist-454ba.firebaseio.com",
-    projectId: "movielist-454ba",
-    storageBucket: "movielist-454ba.appspot.com",
-    messagingSenderId: "444347257371",
-    appId: "1:444347257371:web:080d977ab1da9906"
-  };
-firebase.initializeApp(firebaseConfig);
-//firebase.auth.Auth.Persistence.
-
-console.log(firebaseConfig);
-
-
 var searchdiv = document.getElementById("search");
 searchdiv.addEventListener('click',function (e) {
 	console.log("search click");
@@ -37,9 +22,4 @@ profilediv.addEventListener('mouseout',function (e) {
 	profilediv.style.backgroundImage = "url('./profile/profile1.png')";
 });
 
-var hey = firebase.database().ref('/').once('value',function(snapshot){
-	var help = snapshot.val();
-	console.log(help.MovieList);
-	
-});
-console.log(hey);
+
